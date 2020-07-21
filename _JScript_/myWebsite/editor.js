a8c6cbc5d6119c5f814c1aa43761cd19fc99cea2
silasngozi <silasngoziechegini@gmail.com>
2020-07-21 15:40:21 -0400

    my website

---
+++ _JScript_/myWebsite/editor.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+// When the user clicks on div, open the popup
+function pop_up() {
+  var id = document.getElementById("myPopup");
+  id.classList.toggle("show");
+}
+
+function hide() {
+  var id = document.getElementById("myPopup");
+  id.classList.toggle("hide");
+}
+
+function aboutMe() {
+  var id = document.getElementById("aboutMe");
+  id.classList.toggle("show");
+}
