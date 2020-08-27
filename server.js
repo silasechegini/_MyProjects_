58e95a2d8ea3426335aa677f159e0172f05cf9a8
Ngozi Silas <silasngoziechegini@gmail.com>
2020-08-27 10:36:21 -0400

    edited form

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-// var express = require('express');
-// var app = express();
-
-// //set port
-// var port = process.env.PORT || 8080
-
-// app.use(express.static(__dirname));
-
-// //routes
-// app.get("/", function(req, res) {
-//     res.render("index");
-// })
-
-// app.listen(port, function(){
-//     console.log("app running");
-// })
-
 const express = require('express');
 var app = express();
 var pg = require('pg');
@@ -29,7 +12,7 @@ app.use(express.static(__dirname ));
 
 //routes
 app.get('/', function (req, res) {
-    res.render("index");
+    res.render("form.html");
 });
 
 
@@ -56,53 +39,9 @@ app.post('/submit', function (req, res) {
             console.log("table created");
         });
     });
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('form.html', { root: __dirname });
 });
 
 app.listen(port, function(){
     console.log("app running");
 })
-
-// router.post('/api/v1/todos', (req, res, next) => {
-//     const results = [];
-//     // Grab data from http request
-//     const data = {firstname: req.body.firstname,
-//         lastname: req.body.lastname,
-//         mail: req.body.mail,
-//         country: req.body.country,
-//         zipcode: req.body.zipcode,
-//         cityName: req.body.cityName,
-//         google: req.body.google,
-//         twitter: req.body.twitter,
-//         linkdin: req.body.linkdin,
-//         facebook: req.body.facebook,
-//         comm: req.body.comm
-//     };
-//     // Get a Postgres client from the connection pool
-//     pg.connect(connectionString, (err, client, done) => {
-//       // Handle connection errors
-//       if(err) {
-//         done();
-//         console.log(err);
-//         return res.status(500).json({success: false, data: err});
-//       }
-//       // SQL Query > Insert Data
-//       client.query('INSERT INTO items(text, complete) values($1, $2)',
-//       [data.firstname, data.lastname, data.mail,
-//         data.country, data.zipcode, data.cityName,
-//         data.google, data.twitter, data.linkdin,
-//         data.facebook, data.comm]);
-//       // SQL Query > Select Data
-//       const query = client.query('SELECT * FROM items ORDER BY id ASC');
-//       // Stream results back one row at a time
-//       query.on('row', (row) => {
-//         results.push(row);
-//       });
-//       // After all data is returned, close connection and return results
-//       query.on('end', () => {
-//         done();
-//         return res.json(results);
-//       });
-//     });
-//   });
-
